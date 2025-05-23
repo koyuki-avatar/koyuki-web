bc002750c704c1ca9d02a371dbdf42fb0b28882d
akio <akio@rittai.org>
2025-05-24 03:57:50 +0900

    update arm

--- lib/ControlSender.ts
+++ lib/ControlSender.ts
@@ -42,7 +42,8 @@ export class ControlSender {
 
   private lastsendjson = "";
   private lastsendtime = 0;
-  private sendCtrlBase(xin: number, zin: number) {
+  // xin, zin, ain: -1〜1
+  private sendCtrlBase(xin: number, zin: number, ain: number) {
     if (!this.dataChannel) {
       console.log("sendCtrlBase: datachannel not found");
       return;
@@ -51,13 +52,14 @@ export class ControlSender {
     const xtmp = xin / 3.6;
     const xsend = Math.round(xtmp * this.maxspeedX * 100) / 100;
     const zsend = Math.round(zin * this.maxspeedZ * 100) / 100;
+    const asend = Math.round((ain*0.5+0.5) * 180);
 
     const now = Date.now();
     if ((now - this.lastsendtime) > 300) {
       this.lastsendjson = "";
     }
 
-    const sendjson = JSON.stringify({ x: xsend, z: zsend });
+    const sendjson = JSON.stringify({ x: xsend, z: zsend, a: asend});
     if (sendjson !== this.lastsendjson) {
       // 念のため，2回送る
       this.dataChannel.send(sendjson);
@@ -71,7 +73,12 @@ export class ControlSender {
   private sendFromJoy() {
     const x = this.joy.GetY() / 100;
     const z = -this.joy.GetX() / 100;
-    this.sendCtrlBase(x, z);
+    const a = parseFloat((document.getElementById("arms") as HTMLInputElement).value);
+    if (isNaN(a)) {
+      this.sendCtrlBase(x, z, 0);
+    } else {
+      this.sendCtrlBase(x, z, a);
+    }
   }
 
   private sendFromGamepad() {
@@ -86,18 +93,21 @@ export class ControlSender {
 
     const rawx = gp.axes[0];
     const rawy = gp.axes[1];
+    const rawa = gp.axes[2];
     const x = Math.abs(rawx) < deadzone ? 0 : rawx;
     const y = Math.abs(rawy) < deadzone ? 0 : rawy;
+    const a = Math.abs(rawa) < deadzone ? 0 : rawa;
 
     const kill = gp.buttons[4].pressed || gp.buttons[5].pressed;
     if (kill) {
-      this.sendCtrlBase(0, 0);
+      this.sendCtrlBase(0, 0, a);
     } else {
-      this.sendCtrlBase(-y, -x);
+      this.sendCtrlBase(-y, -x, a);
     }
 
     this.joy.SetXY?.(rawx * 100, -rawy * 100);
     this.updateStatus("Connected: " + gp.id);
+    (document.getElementById("arms") as HTMLInputElement).value = a.toString();
   }
 
   private updateStatus(text: string) {
